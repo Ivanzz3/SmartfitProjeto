0abbc2bde7a2b6f68473317d76e9dd90db98d83e
Ivan <junior.pereira09@aluno.ifce.edu.br>
2024-05-27 15:59:23 -0300

    Smartfit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {GetUnitsService} from "./services/get-units.service";
+import { Location} from "./types/location.interface";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  showList = new BehaviorSubject(false);
+  unitsList : Location [] = [];
+  constructor(private unitService: GetUnitsService) {
+
+  }
+  onSubmit() {
+   this.showList.next(true);
+   this.unitsList = this.unitService.getFilteredUnits( );
+  }
+}
